a61fa495f9bcad3c99d61ce34b567fffb7bb8e3a
Brian Schroeder <bts@gmail.com>
2015-07-07 11:22:40 -0400

    Implement getDeltas, returning a bytestring

--- src/Network/Nylas.hs
+++ src/Network/Nylas.hs
@@ -5,8 +5,9 @@ module Network.Nylas where
 import           Control.Lens
 -- import Pipes
 -- import Pipes.Prelude as P
-import           Data.Aeson.Lens
-import qualified Data.ByteString.Char8 as B
+-- import           Data.Aeson.Lens
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BL
 import           Data.Monoid
 import qualified Data.Text as T
 -- import Data.Text (pack)
@@ -21,13 +22,13 @@ deltaUrl :: Namespace -> Url
 deltaUrl (Namespace n) = "https://api.nylas.com/n/" <> n <> "/delta"
 
 authenticatedOpts :: AccessToken -> Options -> Options
-authenticatedOpts (AccessToken t) = auth ?~ basicAuth (B.pack t) ""
+authenticatedOpts (AccessToken t) = auth ?~ basicAuth (BS.pack t) ""
 
--- getDeltas :: AccessToken -> Namespace -> Cursor -> IO (Response _)
--- getDeltas t n (Cursor c) = getWith opts url
---    where opts = defaults & authenticatedOpts t
---                          & param "cursor" .~ [T.pack c]
---          url = deltaUrl n
+getDeltas :: AccessToken -> Namespace -> Cursor -> IO (Response BL.ByteString)
+getDeltas t n (Cursor c) = getWith opts url
+   where opts = defaults & authenticatedOpts t
+                         & param "cursor" .~ [T.pack c]
+         url = deltaUrl n
 
 main :: IO ()
 main = do
@@ -35,11 +36,12 @@ main = do
   let namespace = Namespace "d1z6pzjd1qvalej8bd51abun9"
   let cursor = Cursor "6h6g1xq4d930ja9375mprv6d0"
 
-  let Cursor c = cursor
+  -- let Cursor c = cursor
+  -- let url = deltaUrl namespace
+  -- let opts = defaults & authenticatedOpts token
+  --                     & param "cursor" .~ [T.pack c]
+  -- r <- getWith opts url
 
-  let url = deltaUrl namespace
-  let opts = defaults & authenticatedOpts token
-                      & param "cursor" .~ [T.pack c]
-  r <- getWith opts url
+  r <- getDeltas token namespace cursor
 
   putStrLn $ show $ r ^. responseBody
